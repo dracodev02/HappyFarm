e36f58254b175e33c8503d6f3043c8bf9e9d1431
quynhuw <qinhuuuuu@gmail.com>
2024-10-18 01:04:17 +0700

    update ui

--- app/components/IntroduceCard.tsx
+++ app/components/IntroduceCard.tsx
@@ -1,14 +1,19 @@
 const IntroduceCard = ({ img, title }: { img: string; title: string }) => {
   return (
-    <div className="flex flex-col flex-1 max-md:max-w-[500px] hover:-translate-y-4 transition-all">
+    <div className="flex w-full flex-col flex-1 max-md:max-w-[500px] max-md:hover:-translate-y-1 hover:-translate-y-4 transition-all">
       <div className="bg-white p-[2px] rounded-[6px]">
-        <div className=" border-[4px] border-b-[14px] aspect-video w-full h-full border-green rounded">
+        <img
+          className=" object-cover border-[4px] border-b-[14px] aspect-video w-full h-full border-green rounded"
+          src={img}
+          alt=""
+        />
+        {/* <div className=" border-[4px] overflow-hidden border-b-[14px] aspect-video w-full h-full border-green rounded">
           <img className="w-full h-full object-cover" src={img} alt="" />
-        </div>
+        </div> */}
       </div>
       <div className="relative text-center max-md:text-[24px] text-[32px]">
         <p className=" leading-8 text-white">{title}</p>
-        <p className=" h-full w-full -z-[1] py-2 leading-8 text-green-yellow absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <p className="h-full select-none max-md:py-1 w-full -z-[1] py-2 leading-8 text-green-yellow absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           {title}
         </p>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -18,25 +18,36 @@ export default function Home() {
   return (
     <div className="">
       <div className="flex flex-col absolute top-0 -z-[2] left-0">
-        <img
+        {/* <img
           className="object-cover h-screen w-screen"
           src="/happyfarm.png"
           alt=""
-        />
+        /> */}
+        <video autoPlay muted loop className="object-cover h-screen w-screen">
+          <source src="/LandingPage.mp4" type="video/mp4" />
+        </video>
       </div>
 
       <div className="items-center pb-2 backdrop-blur-[2px] text-center mt-[200px] max-lg:mt-[200px] flex flex-col gap-8 max-w-desktop w-full mx-auto px-2">
-        <img
+        {/* <img
           className="md:hidden w-1/2 max-w-[200px] object-cover"
           src="/logo.png"
           alt=""
-        />
+        /> */}
+        <video
+          autoPlay
+          muted
+          loop
+          className="md:hidden w-1/2 max-w-[200px] object-cover"
+        >
+          <source src="/LandingPage.mp4" type="video/mp4" />
+        </video>
         <div className="relative max-md:text-[50px] text-[72px] leading-[72px] max-md:leading-[50px]">
           <h1 className="uppercase inline-grid text-white">
             <span>experience an interesting</span>
             <span>{`way to explore the farmer's life`}</span>
           </h1>
-          <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full py-2 -z-[1] uppercase inline-grid text-black">
+          <h1 className="absolute select-none top-1/2 left-1/2 max-md:py-1.5 -translate-x-1/2 -translate-y-1/2 w-full h-full py-2 -z-[1] uppercase inline-grid text-black">
             <span>experience an interesting</span>
             <span>{`way to explore the farmer's life`}</span>
           </h1>
@@ -47,7 +58,7 @@ export default function Home() {
             <br />
             Master skills and play with friends. Build new communities
           </p>
-          <p className="text-[24px] leading-7 py-1.5 -z-[1] text-black absolute top-1/2 left-1/2 w-full h-full -translate-x-1/2 -translate-y-1/2">
+          <p className="text-[24px] select-none leading-7 py-1.5 max-md:py-1 -z-[1] text-black absolute top-1/2 left-1/2 w-full h-full -translate-x-1/2 -translate-y-1/2">
             Make your home in a world of unlimited adventure.
             <br />
             Master skills and play with friends. Build new communities
