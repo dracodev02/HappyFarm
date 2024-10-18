5d0d49900445b65bb16f36cc1bdc8800171966dd
giabaoitwork <139120902+dracodev02@users.noreply.github.com>
2024-10-18 12:16:40 +0700

    Update IntroduceCard.tsx

--- app/components/IntroduceCard.tsx
+++ app/components/IntroduceCard.tsx
@@ -6,7 +6,7 @@ const IntroduceCard = ({ img, title }: { img: string; title: string }) => {
           <img className="w-full h-full object-cover" src={img} alt="" />
         </div>
       </div>
-      <div className="relative text-center max-md:text-[24px] text-[32px]">
+      <div className="relative text-center max-md:text-[24px] text-[32px] backdrop-blur-[2px] pb-2">
         <p className=" leading-8 text-white">{title}</p>
         <p className=" h-full w-full -z-[1] py-2 leading-8 text-green-yellow absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           {title}
